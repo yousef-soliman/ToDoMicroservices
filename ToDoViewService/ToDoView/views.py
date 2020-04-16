17537f0d9dabafe2fd640074220618e993192782
Yousef Soliman <yousef.soliman02@gmail.com>
2020-04-17 00:41:03 +0200

    [ADD]ToDoViewService: add an example of kafka

--- ToDoViewService/ToDoView/views.py
+++ ToDoViewService/ToDoView/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
+from confluent_kafka import Producer
 
+import socket
+import logging
+logger = logging.getLogger(__name__)
+def acked(err, msg):
+    if err is not None:
+        logger.info("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        logger.info("Message produced: %s" % (str(msg)))
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    conf = {'bootstrap.servers': "kafka:9092",
+        'client.id': socket.gethostname()}
+    producer = Producer(conf)
+    logger.info(" %s" % (str(producer)))
+    message = producer.produce("test", key="key", value="value", callback=acked)
+    producer.flush()
+    return HttpResponse("Hello, world. You're at the polls " +
+                        str(message))
 
